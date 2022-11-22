dbee5a7f9b68efea2c9d80d28d205e60d6f1239b
pedrofranca22 <101815535+pedrofranca22@users.noreply.github.com>
2022-11-22 20:10:13 -0300

    configrute viewls
    descricao models create

--- conexao_firebase/app/views.py
+++ conexao_firebase/app/views.py
@@ -6,21 +6,33 @@ from app.models import questEconomico
 def resultado(request):
     
     if request.method == "POST":
-        tipo_bolsa = request.POST.get('tipo_bolsa')
-        nome_completo = request.POST.get('nome_completo')
+        modalidade = request.POST.get('modalidade')
+        nome_estudante = request.POST.get('nome_estudante')
+        matricula = request.POST.get('matricula')
+        genero = request.POST.get('genero')
         data_nascimento = request.POST.get('data_nascimento')
+        curso = request.POST.get('curso')
+        Identidade = request.POST.get('Identidade')
+        renumeracao = request.POST.get('renumeracao')
+        nome_Completo = request.POST.get('nome_Completo')
         cpf = request.POST.get('cpf')
+        Data_Nascimento = request.POST.get('Data_Nascimento')
         endereco = request.POST.get('endereco')
         nrTelCelular = request.POST.get('nrTelCelular')
-        arquivo = request.FILES["arquivo"]
         ##arquivo.save()
-        questEconomico.objects.create(tipo_bolsa = tipo_bolsa,
-                                      nome_completo = nome_completo,
-                                      data_nascimento = data_nascimento,
-                                      cpf = cpf,
-                                      endereco = endereco,
-                                      nrTelCelular = nrTelCelular,
-                                      arquivo = arquivo)
+        questEconomico.objects.create(modalidade = modalidade,
+                                      nome_estudante = nome_estudante ,
+                                      matricula = matricula ,
+                                      genero = genero ,
+                                      data_nascimento = data_nascimento ,
+                                      curso = curso ,
+                                      Identidade = Identidade ,
+                                      renumeracao = renumeracao ,
+                                      nome_Completo = nome_Completo ,
+                                      cpf = cpf ,
+                                      Data_Nascimento = Data_Nascimento ,
+                                      endereco = endereco ,
+                                      nrTelCelular = nrTelCelular)
         return HttpResponse('ARQUIVO ENVIANDO COM SUCESSO')
 
     return redirect('/resultados')
